5b3b817370a43a9345e8c3b2633099a9d1b98041
Michael Behrens <behrens@ou.edu>
2024-04-25 17:02:14 -0500

    add new

---
+++ blib/src/bstructures.h
@@ -0,0 +1,32 @@
+/**
+ * @file bstructures.h
+ * @author baron (admin@baron.rip)
+ * @brief Contains implementations of a number of data structures
+ * @version 0.1
+ * @date 2024-04-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ * 
+ * List of implemented structures (so far):
+ * 
+ * 
+ * 
+ * List of structures to implement:
+ * Linked list (Singly, circular, doubly)
+ * Queue
+ * Stack
+ * 
+ * Tree (Binary, BST)
+ * Graph
+ * Hash table
+ * 
+ */
+#ifndef BSTRUCTURES_H
+#define BSTRUCTURES_H
+
+
+
+
+
+#endif
