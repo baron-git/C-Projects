90d67c0a2d37bab37aa753a50d32e1a461587cb1
Michael Behrens <behrens@ou.edu>
2024-04-25 17:34:58 -0500

    bstructures now uses btemplate, tester now uses binclude to simplify testing

--- blib/src/bstructures.h
+++ blib/src/bstructures.h
@@ -26,9 +26,49 @@
 #define BSTRUCTURES_H
 
 
-int add_one(int a) {
-    return a + 1;
+#ifdef __cplusplus
+extern "C" {  // ensures that the functions are exposed as C functions when included in C++ projects.
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define public macros and constants
+#define MYLIB_VERSION "1.0"
+
+// Define public types and structs
+typedef struct {
+    int id;
+    char *name;
+} MyStruct;
+
+// Declaration of a static function (only visible in this translation unit)
+static void privateFunction(const char *message) { // privateFunction is declared as static, which means it can only be 
+    printf("Private Message: %s\n", message);      // called within this file. This helps to encapsulate the functionality and keep it private to the implementation.
 }
 
+// Public API function declarations
+void myPublicFunction(int data);
 
+// Inline function definitions
+static inline void myInlineFunction(int data) {
+    printf("Inline Function Called with: %d\n", data);
+}
+
+// Function implementations
+static void myStaticFunctionImplementation(int count) {
+    for (int i = 0; i < count; i++) {
+        privateFunction("Looping!");
+    }
+}
+
+void myPublicFunction(int data) {
+    printf("Public Function Called with: %d\n", data);
+    myStaticFunctionImplementation(data);
+}
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif // BSTRUCTURES_H
--- blib/src/test.c
+++ blib/src/test.c
@@ -1,5 +1,6 @@
 #include "binclude.h"
 
+
 int main() {
     
 }
